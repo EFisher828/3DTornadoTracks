42d9e25afec9375d918f53e9740357ad762200a9
EFisher828 <71984782+EFisher828@users.noreply.github.com>
2021-05-19 19:35:47 -0400

    24-hr change

--- stationPull.js
+++ stationPull.js
@@ -8,15 +8,16 @@ const month = monthConversion(dateList[1])
 const day = dateList[2]
 const year = dateList[3]
 
-//Day less for iframe
-const dayLess = day - 1
-
 const timeFull = dateList[4]
 const timeList = timeFull.split(':')
 const hour = timeList[0]
 const minuteEnd = round5(timeList[1])
 const minuteStart = lessen(minuteEnd)
 const hourStart = hourFunction(minuteStart,hour)
+
+//Day less for iframe
+const dayLess = day - 1
+const hourLess = hour - 1
 
 let url = `https://api.climate.ncsu.edu/data?loc=GRANDFATHR&var=airtemp10m,dewtemp10m,gustspeed10m,windspeed10m&start=${year}-${month}-${day}%20${hourStart}:${minuteStart}&end=${year}-${month}-${day}%20${hour}:${minuteEnd}&int=5%20MINUTE&obtype=O&output=json&attr=location,datetime,var,value,unit,obtime&hash=dfbe688cda05afac338c1541f177bc1019f89435`
 console.log(url)
@@ -57,12 +58,11 @@ xhr.onreadystatechange = function() {
 		}						
 		
 		renderResponse(time,temperature,dew,gust,speed)				
-		//console.log(result)
 	}
 }
 					
 xhr.open("GET",url)
 xhr.send()
 
-let tempDewFrame = document.getElementById('tempDew').src = `https://api.climate.ncsu.edu/data?loc=GRANDFATHR&var=airtemp10m,dewtemp10m&start=${year}-${month}-${dayLess}%20${hour}:00&end=${year}-${month}-${day}%20${hour}:00&int=1%20HOUR&obtype=H&output=chart&attr=location,datetime,var,value,value_accum,unit,score,nettype,paramtype,obtype,obnum,obtime&hash=dfbe688cda05afac338c1541f177bc1019f89435`
-let speedGustFrame = document.getElementById('speedGust').src = `https://api.climate.ncsu.edu/data?loc=GRANDFATHR&var=windspeed10m,gustspeed10m&start=${year}-${month}-${dayLess}%20${hour}:00&end=${year}-${month}-${day}%20${hour}:00&int=1%20HOUR&obtype=H&output=chart&attr=location,datetime,var,value,value_accum,unit,score,nettype,paramtype,obtype,obnum,obtime&hash=dfbe688cda05afac338c1541f177bc1019f89435`
+let tempDewFrame = document.getElementById('tempDew').src = `https://api.climate.ncsu.edu/data?loc=GRANDFATHR&var=airtemp10m,dewtemp10m&start=${year}-${month}-${dayLess}%20${hourLess}:00&end=${year}-${month}-${day}%20${hourLess}:00&int=1%20HOUR&obtype=H&output=chart&attr=location,datetime,var,value,value_accum,unit,score,nettype,paramtype,obtype,obnum,obtime&hash=dfbe688cda05afac338c1541f177bc1019f89435`
+let speedGustFrame = document.getElementById('speedGust').src = `https://api.climate.ncsu.edu/data?loc=GRANDFATHR&var=windspeed10m,gustspeed10m&start=${year}-${month}-${dayLess}%20${hourLess}:00&end=${year}-${month}-${day}%20${hourLess}:00&int=1%20HOUR&obtype=H&output=chart&attr=location,datetime,var,value,value_accum,unit,score,nettype,paramtype,obtype,obnum,obtime&hash=dfbe688cda05afac338c1541f177bc1019f89435`
